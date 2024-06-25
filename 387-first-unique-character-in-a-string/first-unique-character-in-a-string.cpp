6fbae6245463a794c7f4c3052284598e42d0f545
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2024-06-25 21:16:29 +0530

    Time: 11 ms (98.97%) | Memory: 12.7 MB (92.72%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    int findUniqueCharIndex(string& s) {
-        int freq[26] = {0};
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s[i];
-            // Now we will update the frquency corresponding to a character
-            freq[ch - 'a']++;
+    int UniqueCharacterIndex(string&s){
+        int size=s.length();
+        //we will store the frequency of each character in frq array
+        //the character having frequency == 1 will be unique
+        int freq[26]={0};
+        for(int i=0;i<size;i++){
+            char ch=s[i];
+            //updating frequency of curr character
+            freq[ch-'a']++;
         }
-        // here our freuqncy table has been updated
-        // Now we will check answers in it
-        // when we get freq[i] = 1 for the first time we will return that index
-        for (int i = 0; i < s.length(); i++) {
+
+        //Now out frequency array is processed
+        //Now checking unique character
+        for(int i=0;i<size;i++){
             char ch = s[i];
-            if (freq[ch - 'a'] == 1) {
-                // since it has value 1 , it is our answer
-                // return its index
+            if(freq[ch-'a'] == 1){
+                //this is unique character
+                //this is our requeired character
+                //so we will return its index
                 return i;
             }
         }
-        // if we are here it means nothing has been returned yet
-        // we neew to return -1 in this case
-        // because there is no unique elements in the string
+        //if nothing has been returned yet
+        //this means there is no unique characters in the string
+        //in this case we have to return 0
         return -1;
     }
-
-    int firstUniqChar(string s) { return findUniqueCharIndex(s); }
+    int firstUniqChar(string s) {
+        return UniqueCharacterIndex(s);
+    }
 };
