3b70adaa1228b0aa5de33c1aff2c45688b5f4c08
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2024-07-01 22:48:33 +0530

    Time: 4 ms (77.45%) | Memory: 17.6 MB (11.31%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,3 +1,4 @@
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -6,38 +7,41 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        queue<TreeNode*>q;
-        if(root==nullptr){
+    int Approach1(TreeNode*&root) {
+        queue<TreeNode*> q;
+        if (root == nullptr) {
             return 0;
         }
         q.push(root);
         q.push(nullptr);
-        int Level=1;
+        int Level = 1;
 
-        while(!q.empty()){
-            TreeNode*front=q.front();
+        while (!q.empty()) {
+            TreeNode* front = q.front();
             q.pop();
-            if(front==nullptr){
+            if (front == nullptr) {
                 Level++;
-                if(!q.empty()){
+                if (!q.empty()) {
                     q.push(nullptr);
                 }
-            }
-            else{
-                if(front->left!=nullptr){
+            } else {
+                if (front->left != nullptr) {
                     q.push(front->left);
                 }
-                if(front->right!=nullptr){
+                if (front->right != nullptr) {
                     q.push(front->right);
                 }
             }
         }
-        return Level-1;
+        return Level - 1;
+    }
+    int maxDepth(TreeNode* root) {
+        return Approach1(root);
     }
 };
