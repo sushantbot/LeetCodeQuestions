1740256d4c608065b7f60daaf82db5fbe7cf5cdd
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2024-07-03 10:21:41 +0530

    Time: 10 ms (30.91%) | Memory: 19.7 MB (50.46%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,29 +11,28 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root, int targetSum,int sum){
+    bool solve(TreeNode* &root, int targetSum,int sum){
         if(root==nullptr){
             return false;
         }
         sum+=root->val;
-        if(root->left==nullptr && root->right==nullptr){
-            //i am currently standing at left node
-            //verify if it is equal to target sum or not
+        if(root->left==nullptr&& root->right==nullptr){
+            //we are at the leafNode and need to verify the sum with targetSum
             if(sum==targetSum){
                 return true;
             }
             else
                 return false;
         }
-
         bool leftKaAns=solve(root->left,targetSum,sum);
         bool rightKaAns=solve(root->right,targetSum,sum);
 
         return leftKaAns || rightKaAns;
+
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
         int sum=0;
-        bool ans=solve(root,targetSum,sum);
-        return ans;
+        bool finalans=solve(root,targetSum,sum);
+        return finalans;
     }
 };
