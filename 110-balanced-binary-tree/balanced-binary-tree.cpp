0ca4a62e2f1ee25a05a496bc79e4f07f2d4456e4
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2024-07-08 07:25:12 +0530

    Time: 91 ms (5.02%) | Memory: 21.6 MB (75.67%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode*& root) {
+        // Using recursion to find the height of the tre
+        if (root == nullptr) {
+            return 0;
+        }
+
+        // solving 1 case myself
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        int height = max(leftHeight, rightHeight) + 1;
+
+        return height;
+    }
+    bool isBalanced(TreeNode* root) {
+        // Agar root== null hai to balanced hi hoga
+        if (root == nullptr) {
+            return true;
+        }
+
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        int diff = abs(leftHeight - rightHeight);
+
+        bool currentNodeAns = (diff <= 1);
+
+        //recursive call
+        bool leftAns = isBalanced(root->left);
+        bool rightAns = isBalanced(root->right);
+
+        if(currentNodeAns && leftAns && rightAns){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
