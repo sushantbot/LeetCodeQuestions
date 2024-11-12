921443125d31e031ec63e91385b80177c1a3a88d
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2024-11-13 01:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (45.29%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -6,21 +6,52 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    // int maxDepth(TreeNode* root) {
+    //     //Base case
+    //     if(root == NULL){
+    //         return 0;
+    //     }
+
+    //     int leftkiHeight=maxDepth(root->left);
+    //     int rightkiHeight=maxDepth(root->right);
+
+    //     int ans= max(leftkiHeight,rightkiHeight);
+    //     return ans+1;
+    // }
+
     int maxDepth(TreeNode* root) {
-        //Base case
-        if(root == NULL){
+        queue<TreeNode*> q;
+        if(root==nullptr){
             return 0;
         }
+        q.push(root);
+        q.push(nullptr);
+        int count = 0;
 
-        int leftkiHeight=maxDepth(root->left);
-        int rightkiHeight=maxDepth(root->right);
-
-        int ans= max(leftkiHeight,rightkiHeight);
-        return ans+1;
+        while (!q.empty()) {
+            TreeNode* frontNode = q.front();
+            q.pop();
+            if (frontNode == nullptr) {
+                count++;
+                if (!q.empty()) {
+                    q.push(nullptr);
+                }
+            } else {
+                if (frontNode->left != nullptr) {
+                    q.push(frontNode->left);
+                }
+                if (frontNode->right != nullptr) {
+                    q.push(frontNode->right);
+                }
+            }
+            
+        }
+        return count;
     }
 };
