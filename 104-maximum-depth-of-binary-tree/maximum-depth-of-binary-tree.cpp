2af206801f44113a6c01f5f8d6295c3430cd65cf
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2024-11-13 01:01:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (8.41%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,4 +1,3 @@
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -7,57 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    //Approach 1 (Using LevelOrderTraversal)
-    int Approach1(TreeNode*&root) {
-        queue<TreeNode*> q;
-        if (root == nullptr) {
-            return 0;
-        }
-        q.push(root);
-        q.push(nullptr);
-        int Level = 1;
-
-        while (!q.empty()) {
-            TreeNode* front = q.front();
-            q.pop();
-            if (front == nullptr) {
-                Level++;
-                if (!q.empty()) {
-                    q.push(nullptr);
-                }
-            } else {
-                if (front->left != nullptr) {
-                    q.push(front->left);
-                }
-                if (front->right != nullptr) {
-                    q.push(front->right);
-                }
-            }
-        }
-        return Level - 1;
-    }
-    //Using recursion to find the
-    int Approach2(TreeNode*&root){
-        if(root==nullptr){
-            return 0;
-        }
-
-        //Now using recursion to find the height
-        int leftHeight=Approach2(root->left);
-        int rightHeight=Approach2(root->right);
-        //Now we will take the maximum of left and right
-        // subtree and store it in the height
-        int height=max(leftHeight,rightHeight)+1;
-        return height;
-    }
     int maxDepth(TreeNode* root) {
-        // return Approach1(root);
-        return Approach2(root);
+        //Base case
+        if(root == NULL){
+            return 0;
+        }
+
+        int leftkiHeight=maxDepth(root->left);
+        int rightkiHeight=maxDepth(root->right);
+
+        int ans= max(leftkiHeight,rightkiHeight);
+        return ans+1;
     }
 };
