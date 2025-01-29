fd3c4eb5514216e587ae6fc67c1458d0b9f34cea
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2025-01-30 03:19:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (75.82%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -13,13 +13,46 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (root == NULL) {
+        // if (root == NULL) {
+        //     return 0;
+        // }
+
+        // int leftAns = maxDepth(root->left);
+        // int rightAns = maxDepth(root->right);
+        // int height = max(leftAns, rightAns) + 1;
+        // return height;
+        if(root==nullptr){
             return 0;
         }
 
-        int leftAns = maxDepth(root->left);
-        int rightAns = maxDepth(root->right);
-        int height = max(leftAns, rightAns) + 1;
-        return height;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(nullptr);
+        int count = 0;
+        while (!q.empty()) {
+            TreeNode* front = q.front();
+            q.pop();
+            if (front == nullptr) {
+                // if front is nullptr then we need to increase the count
+                count++;
+                if(!q.empty()){
+                    //nullptr tabhi push krenge jab q empty nahi hoha
+                    //agar q empty ho gya aur nullptr ko pass kr diye to
+                    //infinite loop me fas jayega
+                    q.push(nullptr);
+                }
+            }
+            else{
+                //we need to push the left and right nodes of the root node
+                //for the further counting of the Levels
+                if(front->left!=nullptr){
+                    q.push(front->left);
+                }
+                if(front->right!=nullptr){
+                    q.push(front->right);
+                }
+            }
+        }
+        return count;
     }
 };
