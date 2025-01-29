9af7b8fb4765670bcc5f1543603517cf7447fb4e
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2025-01-30 03:05:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (95.52%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,46 +12,14 @@
  */
 class Solution {
 public:
-    // int maxDepth(TreeNode* root) {
-    //     //Base case
-    //     if(root == NULL){
-    //         return 0;
-    //     }
-
-    //     int leftkiHeight=maxDepth(root->left);
-    //     int rightkiHeight=maxDepth(root->right);
-
-    //     int ans= max(leftkiHeight,rightkiHeight);
-    //     return ans+1;
-    // }
-
     int maxDepth(TreeNode* root) {
-        queue<TreeNode*> q;
-        if(root==nullptr){
+        if (root == NULL) {
             return 0;
         }
-        q.push(root);
-        q.push(nullptr);
-        int count = 0;
 
-        while (!q.empty()) {
-            TreeNode* frontNode = q.front();
-            q.pop();
-            if (frontNode == nullptr) {
-                count++;
-                if (!q.empty()) {
-                    q.push(nullptr);
-                }
-            } else {
-                if (frontNode->left != nullptr) {
-                    q.push(frontNode->left);
-                }
-                if (frontNode->right != nullptr) {
-                    q.push(frontNode->right);
-                }
-            }
-            
-        }
-        return count;
+        int leftAns = maxDepth(root->left);
+        int rightAns = maxDepth(root->right);
+        int height = max(leftAns, rightAns) + 1;
+        return height;
     }
 };
