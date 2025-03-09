dd0438d3d85636ad9c4eb505989c35354989f000
SUSHANT KUMAR <135549073+ksushantt72@users.noreply.github.com>
2025-03-09 16:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (45.16%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -6,53 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        // if (root == NULL) {
-        //     return 0;
-        // }
-
-        // int leftAns = maxDepth(root->left);
-        // int rightAns = maxDepth(root->right);
-        // int height = max(leftAns, rightAns) + 1;
-        // return height;
         if(root==nullptr){
             return 0;
         }
 
-        queue<TreeNode*> q;
-        q.push(root);
-        q.push(nullptr);
-        int count = 0;
-        while (!q.empty()) {
-            TreeNode* front = q.front();
-            q.pop();
-            if (front == nullptr) {
-                // if front is nullptr then we need to increase the count
-                count++;
-                if(!q.empty()){
-                    //nullptr tabhi push krenge jab q empty nahi hoha
-                    //agar q empty ho gya aur nullptr ko pass kr diye to
-                    //infinite loop me fas jayega
-                    q.push(nullptr);
-                }
-            }
-            else{
-                //we need to push the left and right nodes of the root node
-                //for the further counting of the Levels
-                if(front->left!=nullptr){
-                    q.push(front->left);
-                }
-                if(front->right!=nullptr){
-                    q.push(front->right);
-                }
-            }
-        }
-        return count;
+        //agar root null nahi hai to recursion se left aur right ke ans
+        //NIkalenge
+
+        int leftAns=maxDepth(root->left);
+        int rightAns=maxDepth(root->right);
+
+        int recAns = max(leftAns,rightAns)+1;
+        return recAns;
     }
 };
