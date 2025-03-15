a64c5e17a79cb029121d19100245b723c2bb8752
SUSHANT KUMAR <135549073+sushantbot@users.noreply.github.com>
2025-03-15 22:06:36 +0530

    Time: 273 ms (12.04%) | Memory: 23.9 MB (12.17%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,34 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    //Function declaration for finding the combined height
-    int height(TreeNode*&root){
-        if(root==nullptr){
+    int height(TreeNode* root){
+        if(root == NULL){
             return 0;
         }
+        //if not we will be applying recursion here
+        int option1 = height(root->left);
+        int option2 = height(root->right);
 
-        int leftHeight=height(root->left);
-        int rightHeight=height(root->right);
-        int height=max(leftHeight,rightHeight)+1;
-        return height;
+        int heightt = max(option1,option2)+1;
+        return heightt;
+
     }
-
     int diameterOfBinaryTree(TreeNode* root) {
-        if (root == nullptr) {
+        if(root==NULL){
             return 0;
         }
-        // first of all we will find the height of left
-        int case1 = diameterOfBinaryTree(root->left);
-        int case2 = diameterOfBinaryTree(root->right);
-        int case3 = height(root->left)+height(root->right);
 
-        //Our answer will be the max of the above three cases
-        return max(case1,max(case2,case3));
+        //here we have 3 case
+        // 1. Either our diameter is in left subtree
+        int option1 = diameterOfBinaryTree(root->left);
+        //Either out diameter is in right subtree
+        int option2 = diameterOfBinaryTree(root->right);
+
+        //3. it may be possible that out diameter is combination of left and right
+        int option3 = height(root->left)+height(root->right);
+
+        //But the answer would be the max of option 1 2 and 3
+        int finalans = max(option1,max(option2,option3));
+        return finalans;
     }
 };
