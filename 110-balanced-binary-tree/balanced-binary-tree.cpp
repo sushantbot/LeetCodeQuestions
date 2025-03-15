0ff7171c7a6e376241cfb6c51f5dac52b97ce615
SUSHANT KUMAR <135549073+sushantbot@users.noreply.github.com>
2025-03-15 23:28:38 +0530

    Time: 83 ms (5.74%) | Memory: 23.2 MB (6.68%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,46 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode*& root) {
-        // Using recursion to find the height of the tre
-        if (root == nullptr) {
+    int height(TreeNode*root){
+        //Base case
+        if(root==NULL){
             return 0;
         }
 
-        // solving 1 case myself
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-        int height = max(leftHeight, rightHeight) + 1;
+        int option1 = height(root->left);
+        int option2 = height(root->right);
 
-        return height;
+        int finalans=max(option1,option2)+1;
+        return finalans;
     }
     bool isBalanced(TreeNode* root) {
-        // Agar root== null hai to balanced hi hoga
-        if (root == nullptr) {
+        if(root==NULL){
+            //in this case we will consider it balanced
             return true;
         }
 
         int leftHeight = height(root->left);
         int rightHeight = height(root->right);
-        int diff = abs(leftHeight - rightHeight);
+        int diff = abs(leftHeight-rightHeight);
 
-        bool diffCheck = (diff <= 1);
+        bool currNodeAns = (diff<=1);
+        bool leftKaAns = isBalanced(root->left);
+        bool rightKaAns = isBalanced(root->right); 
 
-        //recursive call
-        bool leftAns = isBalanced(root->left);
-        bool rightAns = isBalanced(root->right);
-
-        if(diffCheck && leftAns && rightAns){
+        //these all three cases in bool need to be true at the same time
+        if(currNodeAns && leftKaAns && rightKaAns){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
+
     }
 };
